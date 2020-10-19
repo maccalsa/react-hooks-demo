80d2968bccb85feae2d5f24aa0cf2aad515d698b
Stuart <maccalsa@googlemail.com>
2020-10-19 13:34:17 +0000

    useEffect

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ const App = () => {
     email: '',
     password: '',
   })
+
+  // when this component is rerendered this will get called.
+  useEffect(() => {
+    effect
+    // this will cleanup the component (if required on unmount)
+    // For example say on render we subscribe to a listenener. here you would unsubscribe
+    return () => {
+      cleanup
+    }
+    // this is how you get the component to render less than its, default which is (EVERY TIME SOMETHING HAPPENS)
+  }, [input])
+
   return (
     <div>
       <input name="email" value={email} onChange={handleChange} />
