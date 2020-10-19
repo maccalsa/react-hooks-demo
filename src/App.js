76b1f1d578469340f20264f02b1d7e50c347e9ba
Stuart <maccalsa@googlemail.com>
2020-10-19 13:09:00 +0000

    useState example 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const expensiveInitialState = () => {
+  //expensive work
+  return 10
 }
 
-export default App;
+const App = () => {
+  const [state, setstate] = useState(10)
+
+  // this will be called the 1st time, and not every time the component remders
+  const [state2, setstate2] = useState(() => expensiveInitialState)
+
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      {/* The way below is a little safer, as it prevents race conditions if you have multiple events that can change 'count*/}
+      <button onClick={() => setCount((currentCount) => currentCount + 1)}>
+        +
+      </button>
+      <div>{count}</div>
+    </div>
+  )
+}
+
+export default App
